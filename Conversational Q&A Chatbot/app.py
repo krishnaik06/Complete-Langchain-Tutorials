2cf1052e6c0d0d713a70147b5e55e01121d92b1f
Krish C Naik <krishnaik06@gmail.com>
2023-11-05 19:13:48 +0530

    Add files via upload

---
+++ Conversational Q&A Chatbot/app.py
@@ -0,0 +1,40 @@
+## Conversational Q&A Chatbot
+import streamlit as st
+
+from langchain.schema import HumanMessage,SystemMessage,AIMessage
+from langchain.chat_models import ChatOpenAI
+
+## Streamlit UI
+st.set_page_config(page_title="Conversational Q&A Chatbot")
+st.header("Hey, Let's Chat")
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+chat=ChatOpenAI(temperature=0.5)
+
+if 'flowmessages' not in st.session_state:
+    st.session_state['flowmessages']=[
+        SystemMessage(content="Yor are a comedian AI assitant")
+    ]
+
+## Function to load OpenAI model and get respones
+
+def get_chatmodel_response(question):
+
+    st.session_state['flowmessages'].append(HumanMessage(content=question))
+    answer=chat(st.session_state['flowmessages'])
+    st.session_state['flowmessages'].append(AIMessage(content=answer.content))
+    return answer.content
+
+input=st.text_input("Input: ",key="input")
+response=get_chatmodel_response(input)
+
+submit=st.button("Ask the question")
+
+## If ask button is clicked
+
+if submit:
+    st.subheader("The Response is")
+    st.write(response)
