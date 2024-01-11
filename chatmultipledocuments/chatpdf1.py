0d82ebccce5ac03a37506f5d8d96740281fbf72e
Krish C Naik <krishnaik06@gmail.com>
2024-01-11 16:10:58 +0530

    Update chatpdf1.py

--- chatmultipledocuments/chatpdf1.py
+++ chatmultipledocuments/chatpdf1.py
@@ -30,7 +30,7 @@ def get_pdf_text(pdf_docs):
 
 
 def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
     chunks = text_splitter.split_text(text)
     return chunks
 
